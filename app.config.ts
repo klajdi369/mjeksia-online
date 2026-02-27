cacaedc332ca9f8fb89d47fdc0baa0291db2af8e
Visar <visarmullaj07@gmail.com>
2026-02-27 18:22:21 +0100

    chore: remove app.json in favor of app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,61 @@
-export default ({ config }) => {
-  return {
-    ...config,
-    extra: {
-      ...config.extra,
-      showDevSettings: process.env.ENVIRONMENT === "dev",
+import { ConfigContext, ExpoConfig } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Mjeksia Online",
+  slug: "mjeksia-online",
+  version: "0.4.1",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "mjeksiaonline",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
     },
-  };
-};
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    package: "com.visarmullaj.mjeksiaonline",
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        dark: {
+          backgroundColor: "#000000",
+        },
+      },
+    ],
+    "expo-sqlite",
+    "expo-font",
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+  extra: {
+    ...config.extra,
+    router: {},
+    eas: {
+      projectId: "ee137e19-390b-497f-b06d-40c7a226fc76",
+    },
+    showDevSettings: process.env.ENVIRONMENT === "dev",
+  },
+});
